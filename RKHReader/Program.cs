21b8da0986e0457488c3bdc67bf1103768489c23
Gustave Monce <gustave.monce@outlook.com>
2022-11-06 23:28:49 +0100

    Initial Code Commit

---
+++ RKHReader/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace RKHReader
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length != 1 || !File.Exists(args[0]))
+            {
+                Console.WriteLine("Usage: <Path to Qualcomm Signed file>");
+            }
+
+            PrintRKHFromFile(args[0]);
+        }
+
+        static void PrintRKHFromFile(string file)
+        {
+            QualcommPartition qualcommPartition = new(file);
+            if (qualcommPartition.RootKeyHash != null)
+            {
+                Console.WriteLine(file);
+                Console.WriteLine("RKH: " + Converter.ConvertHexToString(qualcommPartition.RootKeyHash, ""));
+            }
+        }
+    }
+}
