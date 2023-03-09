82ef5716599bb97237a7f4a26ba9acfcfe00c400
Gustave Monce <gustave.monce@outlook.com>
2023-03-09 21:36:08 +0100

    Improvements

--- RKHReader/Program.cs
+++ RKHReader/Program.cs
@@ -4,21 +4,45 @@
     {
         static void Main(string[] args)
         {
-            if (args.Length != 1 || !File.Exists(args[0]))
+            if (args.Length != 1 || !(File.Exists(args[0]) || Directory.Exists(args[0])))
             {
                 Console.WriteLine("Usage: <Path to Qualcomm Signed file>");
             }
 
-            PrintRKHFromFile(args[0]);
+            if (File.Exists(args[0]))
+            {
+                PrintRKHFromFile(args[0]);
+            }
+
+            if (Directory.Exists(args[0]))
+            {
+                foreach (var el in Directory.EnumerateFiles(args[0], "*.*", SearchOption.AllDirectories))
+                {
+                    PrintRKHFromFile(el);
+                }
+            }
         }
 
         static void PrintRKHFromFile(string file)
         {
-            QualcommPartition qualcommPartition = new(file);
-            if (qualcommPartition.RootKeyHash != null)
+            try
+            {
+                QualcommPartition qualcommPartition = new(file);
+                if (qualcommPartition.RootKeyHash != null)
+                {
+                    Console.WriteLine(file);
+                    Console.WriteLine("RKH: " + Converter.ConvertHexToString(qualcommPartition.RootKeyHash, ""));
+                }
+                else
+                {
+                    Console.WriteLine(file);
+                    Console.WriteLine("FAIL!");
+                }
+            } catch (Exception e)
             {
                 Console.WriteLine(file);
-                Console.WriteLine("RKH: " + Converter.ConvertHexToString(qualcommPartition.RootKeyHash, ""));
+                Console.WriteLine("EXCEPTION!");
+                Console.WriteLine(e);
             }
         }
     }
